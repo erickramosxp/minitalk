1db5aa7d6bcdc14d75f16eb583face4825c576c4
erickramosxp <erickramossilva@gmail.com>
2023-12-29 15:10:32 -0300

    Not work

--- client.c
+++ client.c
@@ -6,51 +6,40 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 18:52:22 by erramos           #+#    #+#             */
-/*   Updated: 2023/12/28 21:18:12 by erramos          ###   ########.fr       */
+/*   Updated: 2023/12/29 15:10:13 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdlib.h>
-void	signal_action(int pid, char *str)
-{
-	int	i;
-	int	c;
 
-	i = 0;
-	while (*str)
-	{
-		c = *(str);
-		while (i < 8)
-		{
-			if (c << i & 0b10000000)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			i++;
-			usleep(150);
-		}
-		str++;
-		i = 0;
-	}
+void    send_string_bit(int pid, char *str)
+{
+        int     i;
+        int     c;
+
+        i = 0;
+        while(*str)
+        {
+                c = *(str);
+                while (i < 8)
+                {
+                        if (c << i & 0b10000000)
+                                kill(pid, SIGUSR1);
+                        else
+                                kill(pid, SIGUSR2);
+                        i++;
+                }
+                i = 0;
+                str++;
+        }
 }
 
-// Validate the number of arguments
-
-// Convert the server ID (PID) to an integer and
-// send the message using the signal_action function
-
-int	main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-	if (argc != 3)
-	{
-		printf("Invalid number of arguments.\n");
-		printf("Format: [./client <SERVER ID (PID)> <STRING>]\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		signal_action(atoi(argv[1]), argv[2]);
-	}
-	return (0);
+        int     a;
+        int     pid;
+
+        pid = atoi(argv[1]);
+        send_string_bit(pid, argv[2]);
 }
--- server.c
+++ server.c
@@ -6,40 +6,39 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 19:21:27 by erramos           #+#    #+#             */
-/*   Updated: 2023/12/28 20:59:12 by erramos          ###   ########.fr       */
+/*   Updated: 2023/12/29 15:09:13 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handler(int sig)
+void    print_num(int bit)
 {
-	static int				i = 0;
-	static unsigned char	c = 0;
+        static int      i;
+        static unsigned char    c;
 
-	if (sig == SIGUSR2)
-		c = c << 1;
-	else if (sig == SIGUSR1)
-		c = (c << 1) | 0b00000001;
-	i++;
-	if (i == 8)
-	{
-		printf("%c", c);
-		i = 0;
-		c = 0;
-	}
+        if (bit == SIGUSR2)
+                c = c << 1;
+        else if (bit == SIGUSR1)
+                c = (c << 1) | 0b00000001;
+        i++;
+        if (i == 8)
+        {
+                printf("%c", c);
+                i = 0;
+                c = 0;
+        }
+
 }
 
-// Print the process ID
-
-// Set up the signal handlers and wait for signals indefinitely
-int	main(void)
+int     main(void)
 {
-	printf("PID: %d\n", getpid());
-	while (1)
-	{
-		signal(SIGUSR1, handler);
-		signal(SIGUSR2, handler);
-	}
-	return (0);
+        printf("%d\n", getpid());
+        while (1)
+        {
+                signal(SIGUSR1, print_num);
+                signal(SIGUSR2, print_num);
+                usleep(200);
+        }
+        return (0);
 }
