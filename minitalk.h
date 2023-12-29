38a079ffb1b72e396d73a822d473972253224cae
erickramosxp <erickramossilva@gmail.com>
2023-12-29 15:11:29 -0300

    Not

--- minitalk.h
+++ minitalk.h
@@ -6,14 +6,13 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 19:24:41 by erramos           #+#    #+#             */
-/*   Updated: 2023/12/28 21:22:54 by erramos          ###   ########.fr       */
+/*   Updated: 2023/12/29 15:11:16 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <bits/types/siginfo_t.h>
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
